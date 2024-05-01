bde68356d2ad89f4f5da82a41bdc54e7e6629d4c
faizaibrahim79 <159462996+faizaibrahim79@users.noreply.github.com>
2024-05-01 21:21:34 +0500

    created word counter project

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// import the inquirer module,which is a command line interface for Node.js
+import inquirer from "inquirer";
+//declare a constant "answers"
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Please provide your sentence for word counting:",
+    },
+]);
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
